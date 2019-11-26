541e9e13143c1ff6f6db46a74584893023216aa5
Jaga Apple <jagaapple@uniboar.com>
2019-11-26 15:35:07 +0900

    [Add] Add `createNosniffHeader` and tests

---
+++ src/nosniff.spec.ts
@@ -0,0 +1,38 @@
+import { createNosniffHeader } from "./nosniff";
+
+describe("createNosniffHeader", () => {
+  describe("return.name", () => {
+    it('should be "X-Content-Type-Options"', () => {
+      expect(createNosniffHeader().name).toBe("X-Content-Type-Options");
+    });
+  });
+
+  describe("return.value", () => {
+    context("when giving undefined", () => {
+      it('should be "nosniff"', () => {
+        expect(createNosniffHeader().value).toBe("nosniff");
+        expect(createNosniffHeader(null as any).value).toBe("nosniff");
+      });
+    });
+
+    context("when giving false", () => {
+      it("should be undefined", () => {
+        expect(createNosniffHeader(false).value).toBe(undefined);
+      });
+    });
+
+    context('when giving "nosniff"', () => {
+      it('should be "nosniff"', () => {
+        expect(createNosniffHeader("nosniff").value).toBe("nosniff");
+      });
+    });
+  });
+
+  context("when giving invalid value", () => {
+    it("should raise error", () => {
+      expect(() => createNosniffHeader(true as any)).toThrow();
+      expect(() => createNosniffHeader("foo" as any)).toThrow();
+      expect(() => createNosniffHeader([] as any)).toThrow();
+    });
+  });
+});
---
+++ src/nosniff.ts
@@ -0,0 +1,20 @@
+import { ResponseHeader } from "./shared";
+
+export type NosniffOption = false | "nosniff";
+
+const headerName = "X-Content-Type-Options";
+const defaultValue: NosniffOption = "nosniff";
+
+const createXContentTypeOptionsHeaderValue = (option?: NosniffOption) => {
+  if (option == undefined) return defaultValue;
+  if (option === false) return;
+  if (option === "nosniff") return defaultValue;
+
+  throw new Error(`Invalid value for ${headerName}: ${option}`);
+};
+
+export const createNosniffHeader = (option?: NosniffOption): ResponseHeader => {
+  const value = createXContentTypeOptionsHeaderValue(option);
+
+  return { name: headerName, value };
+};
