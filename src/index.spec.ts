bdbd3e6b8e3e0a29922551897c97c1a523b1ef75
Jaga Apple <jagaapple@uniboar.com>
2019-11-26 19:34:10 +0900

    [Add] Add `createHeadersObject` and tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,90 @@
-import { returnOne } from "./index";
+import * as rules from "./rules";
+import { createHeadersObject } from "./index";
 
-describe("index.ts", () => {
-  it("should return one", () => {
-    expect(returnOne()).toBe(1);
+describe("createHeadersObject", () => {
+  let forceHTTPSRedirectHeaderCreatorSpy: jest.SpyInstance<
+    ReturnType<typeof rules.createForceHTTPSRedirectHeader>,
+    Parameters<typeof rules.createForceHTTPSRedirectHeader>
+  >;
+  let frameGuardHeaderCreatorSpy: jest.SpyInstance<
+    ReturnType<typeof rules.createFrameGuardHeader>,
+    Parameters<typeof rules.createFrameGuardHeader>
+  >;
+  let noopenHeaderCreatorSpy: jest.SpyInstance<
+    ReturnType<typeof rules.createNoopenHeader>,
+    Parameters<typeof rules.createNoopenHeader>
+  >;
+  let nosniffHeaderCreatorSpy: jest.SpyInstance<
+    ReturnType<typeof rules.createNosniffHeader>,
+    Parameters<typeof rules.createNosniffHeader>
+  >;
+  let xssProtectionHeaderCreatorSpy: jest.SpyInstance<
+    ReturnType<typeof rules.createXSSProtectionHeader>,
+    Parameters<typeof rules.createXSSProtectionHeader>
+  >;
+
+  describe("calling rules", () => {
+    beforeAll(() => {
+      forceHTTPSRedirectHeaderCreatorSpy = jest.spyOn(rules, "createForceHTTPSRedirectHeader");
+      frameGuardHeaderCreatorSpy = jest.spyOn(rules, "createFrameGuardHeader");
+      noopenHeaderCreatorSpy = jest.spyOn(rules, "createNoopenHeader");
+      nosniffHeaderCreatorSpy = jest.spyOn(rules, "createNosniffHeader");
+      xssProtectionHeaderCreatorSpy = jest.spyOn(rules, "createXSSProtectionHeader");
+    });
+
+    it("should call each rules and give proper options", () => {
+      const dummyOptions: Parameters<typeof createHeadersObject>[0] = {
+        forceHTTPSRedirect: [123, { preload: true }],
+        frameGuard: ["allow-from", { uri: "https://example.example.com" }],
+        noopen: false,
+        nosniff: false,
+        xssProtection: ["report", { uri: "https://example.example.com" }],
+      };
+      createHeadersObject(dummyOptions);
+
+      expect(forceHTTPSRedirectHeaderCreatorSpy).toBeCalledTimes(1);
+      expect(forceHTTPSRedirectHeaderCreatorSpy).toBeCalledWith(dummyOptions.forceHTTPSRedirect);
+
+      expect(frameGuardHeaderCreatorSpy).toBeCalledTimes(1);
+      expect(frameGuardHeaderCreatorSpy).toBeCalledWith(dummyOptions.frameGuard);
+
+      expect(noopenHeaderCreatorSpy).toBeCalledTimes(1);
+      expect(noopenHeaderCreatorSpy).toBeCalledWith(dummyOptions.noopen);
+
+      expect(nosniffHeaderCreatorSpy).toBeCalledTimes(1);
+      expect(nosniffHeaderCreatorSpy).toBeCalledWith(dummyOptions.nosniff);
+
+      expect(xssProtectionHeaderCreatorSpy).toBeCalledTimes(1);
+      expect(xssProtectionHeaderCreatorSpy).toBeCalledWith(dummyOptions.xssProtection);
+    });
+  });
+
+  describe("filtering", () => {
+    beforeAll(() => {
+      forceHTTPSRedirectHeaderCreatorSpy = jest.spyOn(rules, "createForceHTTPSRedirectHeader");
+      forceHTTPSRedirectHeaderCreatorSpy.mockReturnValue({ name: "dummy-1", value: undefined });
+
+      frameGuardHeaderCreatorSpy = jest.spyOn(rules, "createFrameGuardHeader");
+      frameGuardHeaderCreatorSpy.mockReturnValue({ name: "dummy-2", value: "example-2" });
+
+      noopenHeaderCreatorSpy = jest.spyOn(rules, "createNoopenHeader");
+      noopenHeaderCreatorSpy.mockReturnValue({ name: "dummy-3", value: undefined });
+
+      nosniffHeaderCreatorSpy = jest.spyOn(rules, "createNosniffHeader");
+      nosniffHeaderCreatorSpy.mockReturnValue({ name: "dummy-4", value: "example-4" });
+
+      xssProtectionHeaderCreatorSpy = jest.spyOn(rules, "createXSSProtectionHeader");
+      xssProtectionHeaderCreatorSpy.mockReturnValue({ name: "dummy-3", value: undefined });
+    });
+
+    it("should omit headers which have undefined value", () => {
+      const returnedHeaders = createHeadersObject();
+
+      expect(returnedHeaders).not.toHaveProperty("dummy-1");
+      expect(returnedHeaders).toHaveProperty("dummy-2", "example-2");
+      expect(returnedHeaders).not.toHaveProperty("dummy-3");
+      expect(returnedHeaders).toHaveProperty("dummy-4", "example-4");
+      expect(returnedHeaders).not.toHaveProperty("dummy-5");
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export const returnOne = () => 1;
+import * as rules from "./rules";
+
+type Options = Partial<{
+  forceHTTPSRedirect: rules.ForceHTTPSRedirectOption;
+  frameGuard: rules.FrameGuardOption;
+  noopen: rules.NoopenOption;
+  nosniff: rules.NosniffOption;
+  xssProtection: rules.XSSProtectionOption;
+}>;
+
+export const createHeadersObject = (options: Options = {}) => {
+  const newHeaders: Record<string, string> = {};
+
+  [
+    rules.createForceHTTPSRedirectHeader(options.forceHTTPSRedirect),
+    rules.createFrameGuardHeader(options.frameGuard),
+    rules.createNoopenHeader(options.noopen),
+    rules.createNosniffHeader(options.nosniff),
+    rules.createXSSProtectionHeader(options.xssProtection),
+  ].forEach((header) => {
+    if (header.value == undefined) return;
+
+    newHeaders[header.name] = header.value;
+  });
+
+  return newHeaders;
+};
