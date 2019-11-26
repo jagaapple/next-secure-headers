9ecdb1d5dc1355e7dfeb8fefc536c40f7729322e
Jaga Apple <jagaapple@uniboar.com>
2019-11-26 15:02:14 +0900

    [Add] Add `createFrameGuardHeader` and tests

---
+++ src/frame-guard.ts
@@ -0,0 +1,25 @@
+import { ResponseHeader } from "./shared";
+
+export type FrameGuardOption = false | "deny" | "sameorigin" | ["allow-from", { uri: string | URL }];
+
+const headerName = "X-Frame-Options";
+const defaultValue: FrameGuardOption = "deny";
+
+const createXFrameOptionsHeaderValue = (option?: FrameGuardOption) => {
+  if (option == undefined) return defaultValue;
+  if (option === false) return;
+  if (option === "deny") return option;
+  if (option === "sameorigin") return option;
+
+  if (Array.isArray(option)) {
+    if (option[0] === "allow-from") return `${option[0]} ${new URL(option[1].uri.toString())}`;
+  }
+
+  throw new Error(`Invalid value for ${headerName}: ${option}`);
+};
+
+export const createFrameGuardHeader = (option?: FrameGuardOption): ResponseHeader => {
+  const value = createXFrameOptionsHeaderValue(option);
+
+  return { name: headerName, value };
+};
