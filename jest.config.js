319f3405e527ad7d4c1ffdc04dba61997e28e9e6
Jaga Apple <jagaapple@uniboar.com>
2019-11-26 14:51:24 +0900

    [Add] Use "jest-plugin-context" package for Jest

--- jest.config.js
+++ jest.config.js
@@ -124,7 +124,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["jest-plugin-context/setup"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
