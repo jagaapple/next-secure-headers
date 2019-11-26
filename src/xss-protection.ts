de162245991341288d92f88f8f937810a2c8bbba
Jaga Apple <jagaapple@uniboar.com>
2019-11-26 15:41:42 +0900

    [Add] Add `createXSSProtectionHeader` and tests

---
+++ src/xss-protection.ts
@@ -0,0 +1,24 @@
+import { ResponseHeader } from "./shared";
+
+export type XSSProtectionOption = false | "sanitize" | "block-rendering" | ["report", { uri: string | URL }];
+
+const headerName = "X-XSS-Protection";
+
+const createXXSSProtectionHeaderValue = (option?: XSSProtectionOption) => {
+  if (option == undefined) return "1";
+  if (option === false) return "0";
+  if (option === "sanitize") return "1";
+  if (option === "block-rendering") return "1; mode=block";
+
+  if (Array.isArray(option)) {
+    if (option[0] === "report") return `1; report=${new URL(option[1].uri.toString())}`;
+  }
+
+  throw new Error(`Invalid value for ${headerName}: ${option}`);
+};
+
+export const createXSSProtectionHeader = (option?: XSSProtectionOption): ResponseHeader => {
+  const value = createXXSSProtectionHeaderValue(option);
+
+  return { name: headerName, value };
+};
