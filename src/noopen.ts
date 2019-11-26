1ed55aa8b4bb4e0d7c179516c06cd5d3106b8958
Jaga Apple <jagaapple@uniboar.com>
2019-11-26 18:32:55 +0900

    [Add] Add `createNoopenHeader` and tests

---
+++ src/noopen.ts
@@ -0,0 +1,20 @@
+import { ResponseHeader } from "./shared";
+
+export type NoopenOption = false | "noopen";
+
+const headerName = "X-Download-Options";
+const defaultValue: NoopenOption = "noopen";
+
+const createXDownloadOptionsHeaderValue = (option?: NoopenOption) => {
+  if (option == undefined) return defaultValue;
+  if (option === false) return;
+  if (option === "noopen") return defaultValue;
+
+  throw new Error(`Invalid value for ${headerName}: ${option}`);
+};
+
+export const createNoopenHeader = (option?: NoopenOption): ResponseHeader => {
+  const value = createXDownloadOptionsHeaderValue(option);
+
+  return { name: headerName, value };
+};
