7d8f082fbbc02ee9a21c15a017b20a30d5d83cd7
Jaga Apple <jagaapple@uniboar.com>
2019-12-04 23:09:08 +0900

    [Modify] Use a CSP rule in the example project

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -5,6 +5,12 @@ import withSecureHeaders from "next-secure-headers";
 class Application extends App {}
 
 export default withSecureHeaders({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: "'self'",
+      styleSrc: ["'self'", "https://stackpath.bootstrapcdn.com"],
+    },
+  },
   forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
   referrerPolicy: "same-origin",
 })(Application);
